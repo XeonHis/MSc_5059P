fd651c40dc54f759dafa79114ffb94a4d5f0cfff
Paul <yingheting@gmail.com>
2022-06-27 02:24:00 +0100

    init align rgb info to depth pic

---
+++ data_process/align.py
@@ -0,0 +1,105 @@
+"""
+Align depth info into color picture
+"""
+# First import library
+import pyrealsense2 as rs
+# Import Numpy for easy array manipulation
+import numpy as np
+# Import OpenCV for easy image rendering
+import cv2
+# Import argparse for command-line options
+import argparse
+# Import os.path for file path manipulation
+import os.path
+
+# Create object for parsing command-line options
+parser = argparse.ArgumentParser(description="Read recorded bag file and display depth stream in jet colormap.\
+                                Remember to change the stream fps and format to match the recorded.")
+# Add argument which takes path to a bag file as an input
+parser.add_argument("-i", "--input", type=str, help="Path to the bag file")
+# Parse the command line arguments to an object
+args = parser.parse_args()
+# set default .bag file
+args.input = "../data/realsense_tissue_roll.bag"
+
+try:
+    # Create pipeline
+    pipeline = rs.pipeline()
+
+    # Create a config object
+    config = rs.config()
+
+    # Tell config that we will use a recorded device from file to be used by the pipeline through playback.
+    rs.config.enable_device_from_file(config, args.input)
+
+    # Configure the pipeline to stream the depth stream
+    # Change this parameters according to the recorded bag file resolution
+    # config.enable_stream(rs.stream.depth, rs.format.z16, 30)
+    # config.enable_stream(rs.stream.color, rs.format.bgr8, 30)
+
+    # Start streaming from file
+    profile = pipeline.start(config)
+
+    # Getting the depth sensor's depth scale (see rs-align example for explanation)
+    depth_sensor = profile.get_device().first_depth_sensor()
+    depth_scale = depth_sensor.get_depth_scale()
+    print("Depth Scale is: ", depth_scale)
+
+    # We will be removing the background of objects more than
+    #  clipping_distance_in_meters meters away
+    clipping_distance_in_meters = 1  # 1 meter
+    clipping_distance = clipping_distance_in_meters / depth_scale
+
+    # Create an align object
+    # rs.align allows us to perform alignment of depth frames to others frames
+    # The "align_to" is the stream type to which we plan to align depth frames.
+    align_to = rs.stream.color
+    align = rs.align(align_to)
+
+    # Create opencv window to render image in
+    cv2.namedWindow("Depth Stream", cv2.WINDOW_AUTOSIZE)
+
+    # Create colorizer object: Colorizer filter generates color images based on input depth frame
+    # colorizer = rs.colorizer()
+
+    # Streaming loop
+    while True:
+        # Get frameset of depth
+        frames = pipeline.wait_for_frames()
+
+        # Align the depth frame to color frame
+        aligned_frames = align.process(frames)
+
+        # Get aligned frames
+        aligned_depth_frame = aligned_frames.get_depth_frame()  # aligned_depth_frame is a 640x480 depth image
+        color_frame = aligned_frames.get_color_frame()
+
+        # Validate that both frames are valid
+        if not aligned_depth_frame or not color_frame:
+            continue
+
+        depth_image = np.asanyarray(aligned_depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        # Remove background - Set pixels further than clipping_distance to grey
+        grey_color = 153
+        depth_image_3d = np.dstack(
+            (depth_image, depth_image, depth_image))  # depth image is 1 channel, color is 3 channels
+        bg_removed = np.where((depth_image_3d <= 0), grey_color, color_image)
+
+        # Render images:
+        #   depth align to color on left
+        #   depth on right
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        images = np.hstack((bg_removed, depth_colormap))
+
+        # Render image in opencv window
+        cv2.imshow("Depth Stream", images)
+        key = cv2.waitKey(1)
+        # if pressed escape exit program
+        if key == 27:
+            cv2.destroyAllWindows()
+            break
+
+finally:
+    pass
