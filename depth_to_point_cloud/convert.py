678f63e9b8991152344bd78f20e7ac6032da1390
Paul <yingheting@gmail.com>
2022-06-28 02:10:57 +0100

    finish

--- depth_to_point_cloud/convert.py
+++ depth_to_point_cloud/convert.py
@@ -2,20 +2,25 @@ import cv2
 import numpy as np
 import open3d as o3d
 from matplotlib import pyplot as plt
+import os
 
-# read image as unchanged
-# image = cv2.imread("../data/processed/realsense_tissue_roll/with_depth/frame_171.png", -1)
-# cv2.imwrite("color.png",image[:,:,:-1].astype(np.uint8))
-# cv2.imwrite("depth.png",image[:,:,-1])
-# print(image[:, :, -1])
-# cv2.imshow("title", image)
-# cv2.waitKey()
+file_dir = "../data/processed/realsense_tissue_roll"
+rgb_image_dir = os.path.join(file_dir, "rgb")
+depth_image_dir = os.path.join(file_dir, "depth")
+pcd_image_dir = os.path.join(file_dir, "pcd")
+if not os.path.exists(pcd_image_dir):
+    os.mkdir(pcd_image_dir)
 
-color_raw = o3d.io.read_image("../data/test_image/color.png")
-depth_raw = o3d.io.read_image("../data/test_image/depth.png")
-rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color=color_raw, depth=depth_raw, depth_scale=1000,
-                                                                depth_trunc=100, convert_rgb_to_intensity=True)
-pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, o3d.camera.PinholeCameraIntrinsic(
-    o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault))
-print(pcd)
-o3d.visualization.draw_geometries([pcd])
+for filename in os.listdir(rgb_image_dir):
+    rgb_raw = o3d.io.read_image(os.path.join(rgb_image_dir, filename))
+    depth_raw = o3d.io.read_image(os.path.join(depth_image_dir, filename))
+    rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color=rgb_raw, depth=depth_raw, depth_scale=1000,
+                                                                    depth_trunc=100, convert_rgb_to_intensity=False)
+    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, o3d.camera.PinholeCameraIntrinsic(
+        o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault))
+    # print(pcd)
+    pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+    # o3d.visualization.draw_geometries([pcd])
+
+    o3d.io.write_point_cloud(os.path.join(pcd_image_dir, filename.replace("png", "pcd")), pcd)
+    # o3d.io.write_point_cloud(os.path.join(pcd_image_dir, filename.replace("png", "xyzrgb")), pcd)
